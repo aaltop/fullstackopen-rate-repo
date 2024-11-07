5626f20e3585a4938424b0069a7a5ef32b8b6570
TA <aaltopi@gmail.com>
2024-11-07 15:41:10 +0200

    Add "environment variables" (10.12)
    Seems to work, but the Expo cache seems to be a bit of a pain,
    or I guess it's something to do with the Metro bundler? At least doing
    "npx expo config" shows the config as updated, but the old content is
    still used when logging in the app unless I --clear, in which
    case it talks about the bundler cache being empty or whatever.
    Annoying, as --clear takes a while to bundle with. Of course,
    shouldn't need to change the env too often, but still.

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -2,11 +2,12 @@ import allFragments from '../grapql/allFragments';
 
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { createFragmentRegistry } from "@apollo/client/cache";
+import env from "../env";
 
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://192.168.68.58:4000/graphql',
+    uri: env.apollo_uri,
     cache: new InMemoryCache({
         fragments: createFragmentRegistry(allFragments)
     }),
